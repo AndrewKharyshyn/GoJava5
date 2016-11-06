c7e5dc767921d1c8c663daeb016d938797f50c11
AndrewKharyshyn <renegaden@ua.fm>
2016-11-06 14:50:27 +0200

    Add files via upload

--- Module_2_2_2/Runner.java
+++ Module_2_2_2/Runner.java
@@ -7,27 +7,11 @@ import java.util.Scanner;
  */
 public class Runner {
     public static void main(String[] args) {
+        Withdrawal withdrawalProcess = new Withdrawal();
 
-        Withdrawal balance = new Withdrawal();
-        Withdrawal withdrawalSum = new Withdrawal();
-
-        System.out.println("Текущий баланс на Вашем счету: " + balance.balance);
-
-        System.out.println("Введите сумму снятия средств...");
-        Scanner sc = new Scanner(System.in);
-        withdrawalSum.withdrawalSum = sc.nextDouble();
-
-        if (withdrawalSum.withdrawalSum >= balance.balance) {
-            withdrawalSum.sumWithdrawal();
-        } else {
-            if (withdrawalSum.withdrawalSum <= 0) {
-                System.out.println("Вы указали неверную сумму. Пожалуйста, повторите ввод...");
-            } else {
-                System.out.println("Вы снимаете: " + withdrawalSum.withdrawalSum + ".");
-                withdrawalSum.sumWithdrawal();
-            }
-        }
+        withdrawalProcess.withdrawalProcess();
     }
 }
+
 
 
--- Module_2_2_2/Withdrawal.java
+++ Module_2_2_2/Withdrawal.java
@@ -1,20 +1,41 @@
 package Module_2_2_2;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Withdrawal {
 
     double balance = 100, withdrawalSum, commission;
 
-    void sumWithdrawal(){
+    void sumWithdrawal() {
 
-        commission = withdrawalSum*0.05;
+        commission = withdrawalSum * 0.05;
 
-        if (balance>=(withdrawalSum+commission)){
+        if (balance >= (withdrawalSum + commission)) {
             balance = balance - withdrawalSum - commission;
-            System.out.println("OK, операция успешно завершена! Снято: " +withdrawalSum+ ". Комиссия за транзакцию: "+commission + " Баланс счета: " + balance);
-        }
-        else {
-            System.out.println("NO, Недостаточно средств на счету (Ваш баланс: "+balance + ". Комиссия за снятие /5%/: " + commission + "). Требуется еще: " + Math.abs(balance-withdrawalSum-commission));
+            System.out.println("OK, операция успешно завершена! Снято: " + withdrawalSum + ". \nКомиссия за транзакцию: " + commission + " Баланс счета: " + balance);
+        } else {
+            System.out.println("NO, Недостаточно средств на счету (Ваш баланс: " + balance + ". Комиссия за снятие /5%/: " + commission + "). Требуется еще: " + Math.abs(balance - withdrawalSum - commission));
         }
     }
 
+    void withdrawalProcess() {
+        System.out.println("Текущий баланс на Вашем счету: " + balance);
+
+        System.out.println("Введите сумму снятия средств...");
+        Scanner sc = new Scanner(System.in);
+        withdrawalSum = sc.nextDouble();
+
+        if (withdrawalSum >= balance) {
+            sumWithdrawal();
+        } else {
+            if (withdrawalSum <= 0) {
+                System.out.println("Вы указали неверную сумму. Пожалуйста, повторите ввод...");
+            } else {
+                System.out.println("Вы снимаете: " + withdrawalSum + ".");
+                sumWithdrawal();
+            }
+        }
+
+    }
 }
