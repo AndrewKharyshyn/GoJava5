dff2dccdd0cc640e38e5da88aec8678d9fe120b6
AndrewKharyshyn <renegaden@ua.fm>
2016-11-07 21:59:50 +0200

    Add files via upload

---
+++ Module_3_3_1/Bird.java
@@ -0,0 +1,11 @@
+package Module_3_3_1;
+
+/**
+ * Created by Andrew on 07.11.2016.
+ */
+public class Bird {
+
+    public void sing (){
+        System.out.println("I am walking\n" + "I am flying\n" + "I am singing\n"+"I am Bird");
+    }
+}
---
+++ Module_3_3_1/BirdAction.java
@@ -0,0 +1,15 @@
+package Module_3_3_1;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Andrew on 07.11.2016.
+ */
+public class BirdAction {
+    public static void main(String[] args) {
+        Bird birdAction = new Bird();
+        birdAction.sing();
+    }
+}
+
+
