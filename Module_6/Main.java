a35520b1a9905fa597ba50406a49d2273b427e72
AndrewKharyshyn <renegaden@ua.fm>
2016-12-06 23:26:34 +0200

    Add files via upload

---
+++ Module_6/Main.java
@@ -0,0 +1,20 @@
+package Module_6;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArraysUtils.digitArray = new int[] {13,72,16, 11, -7, 118,21,8,-6, 64};
+
+        ArraysUtils.result();
+        ArraysUtils.resultmin();
+        ArraysUtils.resultmax();
+        ArraysUtils.resultmaxpos();
+        ArraysUtils.resultmaxneg();
+        ArraysUtils.resultmultip();
+        ArraysUtils.resultmod();
+        ArraysUtils.secondLargNum();
+        ArraysUtils.reverse();
+        ArraysUtils.findEvenElements();
+
+    }
+}
---
+++ Module_6/UserUtils.java
@@ -0,0 +1,24 @@
+package Module_6;
+
+public class UserUtils {
+/*
+    User[] uniqueUsers(User[] users){
+
+    }
+
+    User[] usersWithContitionalBalance(User[] users, int balance){
+
+    }
+
+    public final User[] paySalaryToUsers(User[] users){
+
+    }
+
+    public final long[] getUsersId(User[] users){
+
+    }
+    User[] deleteEmptyUsers(User[] users){
+
+    }
+    */
+}
