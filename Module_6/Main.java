8b6a86b042e4c6ffa32ec750d94c1c4f8374d9d3
AndrewKharyshyn <renegaden@ua.fm>
2016-12-07 01:02:19 +0200

    Add files via upload

--- Module_6/Main.java
+++ Module_6/Main.java
@@ -4,6 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         ArraysUtils.digitArray = new int[] {13,72,16, 11, -7, 118,21,8,-6, 64};
+        User user1 = new User(1, "Davidov", "Ruslan", 5500, 1800);
+        User user2 = new User(2, "Kovalska", "Anna", 2000, 300);
+        User user3 = new User(3, "Maksimchuk", "Iryna", 2500, 1000);
+        User user4 = new User(4, "Fedorchuk", "Natalia", 500, 800);
+        User user5 = new User(5, "Kushnir", "Vitaliy", 1250, 450);
+        User user6 = new User(4, "Fedorchuk", "Natalia", 500, 800);
+
+        User[] users = new User[6];
+        users[0] = user1;
+        users[1] = user2;
+        users[2] = user3;
+        users[3] = user4;
+        users[4] = user5;
+        users[5] = user6;
 
         ArraysUtils.result();
         ArraysUtils.resultmin();
@@ -15,6 +29,10 @@ public class Main {
         ArraysUtils.secondLargNum();
         ArraysUtils.reverse();
         ArraysUtils.findEvenElements();
+        System.out.println();
+
+        UserUtils userUtils = new UserUtils();
+        userUtils.uniqueUsers(users);
 
     }
 }
--- Module_6/UserUtils.java
+++ Module_6/UserUtils.java
@@ -1,11 +1,22 @@
 package Module_6;
 
+import java.util.Arrays;
+
 public class UserUtils {
-/*
-    User[] uniqueUsers(User[] users){
 
+    User[] uniqueUsers(User[] users) {
+        User[] uniqueUsers = new User[users.length];
+        for (int i = 0, j = 0; i < users.length; i++, j++) {
+            uniqueUsers[j] = users[i];
+            if (uniqueUsers[j].equals(users[i])) {
+                uniqueUsers[j] = null;
+                System.out.println(uniqueUsers [j]);
+            }
+        }
+
+        return uniqueUsers;
     }
-
+/*
     User[] usersWithContitionalBalance(User[] users, int balance){
 
     }
